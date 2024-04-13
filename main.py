e51011dc8458bf120bd53cc0f9bad86f0af08d2c
malasiaa <jose.jn@hotmail.com>
2024-04-13 11:39:02 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
+import streamlit
 from transformers import pipeline
 
-classifier = pipeline("sentiment-analysis")
-classifier("I've been waiting for a HuggingFace course my whole life.")
+classifier = pipeline("sentiment-analysis", model="federicopascual/finetuning-sentiment-model-3000-samples")
+results = classifier(
+    ["I've been waiting for a HuggingFace course my whole life.", "I hate this so much!"]
+)
+
+# Print the results
+for result in results:
+    print(result)
